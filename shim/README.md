1e862e934c3ff805c190ee17f8414e2c8a926a33
Brandon Carpenter <brandon@avantus.com>
2024-12-20 14:54:52 -0800

    Shim falls back to running ephemeral runner with uv tool

--- shim/README.md
+++ shim/README.md
@@ -1,13 +1,18 @@
 # pyproject-runner-shim
 
 pyproject-runner-shim provides a tiny convenience script to shorten the command used to execute
-pyproject-runner tasks from `uv run rr TASK ...` to `rr TASK ...`. This assumes the shim is being
-executed from the directory or subdirectory of a uv-managed project with pyproject-runner installed
-in the project's virtual environment.
+pyproject-runner tasks from `uv run rr TASK ...` to `rr TASK ...`.
 
 [pyproject-runner](../README.md) provides a simple, project-oriented method of defining developer scripts.
 It is a simple task runner, similar to [taskipy](https://pypi.org/project/taskipy/) or [Poe the Poet](https://pypi.org/project/poethepoet/), for running tasks
 defined in a *pyproject.toml* file.
+
+Pyproject-runner-shim expects the `rr` shim to be executed from a uv-managed project directory or
+subdirectory, with or without pyproject-runner installed in the project's virtual environment. If
+pyproject-runner is installed in the project's virtual environment, then that version will be run
+using the virtual environment's python executable. Otherwise, it will use `uv tool run` to download
+and run pyproject-runner from an ephemeral environment. This offers flexibility in how projects use
+pyproject-runner.
 
 
 ## Installation
@@ -30,7 +35,9 @@ pyproject-runner package, which uv installs into the uv tool bin directory. Usin
 standard library, the script simply executes `uv run -- python3 -m pyproject_runner`, passing along
 any arguments provided to the script. This allows the shim to be used with any number of uv-based
 projects that may require different versions of pyproject-runner. uv automatically determines which
-virtual environment to used based on the current working directory.
+virtual environment to used based on the current working directory. Or, if pyproject-runner isn't
+a project dependency, then it will be run from an ephemeral virtual environment, allowing it to be
+used with projects requiring python < 3.10.
 
 
 ## Changelog
--- shim/rr.py
+++ shim/rr.py
@@ -1,22 +1,49 @@
-"""Shim for running pyproject-runner directly, without using `uv run`."""
+"""Shim for running pyproject-runner directly, without using `uv run`.
+
+This script is designed to be used with uv. It can be installed in the project
+virtual environment, by including it in the *dev* dependency group in
+*pyproject.toml*, or as a tool, using `uv tool install pyproject-runner-shim`.
+Installing it as a tool allows it to be used with multiple projects, with or
+without installing pyproject-runner in the project's virtual environment. If
+pyproject-runner is installed in the project's virtual environment, then it
+will be loaded and run using the project's python, thus allowing different
+versions of pyproject-runner to be used with different projects and updated
+when syncing the project. Otherwise, it will use `uv tool` to run
+pyproject-runner from en ephemeral virtual environment.
+"""
 
 import os
 import subprocess
 import sys
 
+RUN_KEY = "__PYPROJECT_RUNNER_SHIM_RUN__"
+
 
 def main():
     """Entry point for rr shim."""
-    cmd = ["uv", "run", "--", "python", "-m", "pyproject_runner", *sys.argv[1:]]
+    if len(sys.argv) > 1 and sys.argv[1] == RUN_KEY:
+        del sys.argv[1]
+        try:
+            from pyproject_runner.__main__ import main
+        except ModuleNotFoundError:
+            run(["uv", "tool", "run", "--from", "pyproject-runner", "rr", *sys.argv[1:]])
+        else:
+            main(prog_name="rr")
+    else:
+        run(["uv", "run", "--", "python", __file__, RUN_KEY, *sys.argv[1:]])
+
+
+def run(cmd: list[str]) -> None:
+    """Run the given command."""
     try:
         if sys.platform == "win32":
             sys.exit(subprocess.run(cmd).returncode)  # noqa: S603
         else:
-            os.execvp("uv", cmd)  # noqa: S606, S607
+            os.execvp(cmd[0], cmd)  # noqa: S606
     except KeyboardInterrupt:
         pass
     except OSError as exc:
-        print(f"error: {exc}: uv", file=sys.stderr)  # noqa: T201
+        print(f"error: {exc}: {cmd[0]}", file=sys.stderr)  # noqa: T201
     sys.exit(1)
 
 
